313a988d190b866b334e1fb797fd068c7a829cc4
Sonya <sonixxxks@yandex.ru>
2026-02-21 16:18:19 +0500

    Первая сдача

--- src/components/filtering.js
+++ src/components/filtering.js
@@ -1,14 +1,40 @@
 import {createComparison, defaultRules} from "../lib/compare.js";
 
-// @todo: #4.3 — настроить компаратор
+const compare = createComparison(defaultRules);
 
 export function initFiltering(elements, indexes) {
-    // @todo: #4.1 — заполнить выпадающие списки опциями
+    Object.keys(indexes)
+      .forEach((elementName) => {
+        elements[elementName].append(
+            ...Object.values(indexes[elementName])
+                .map(name => {
+                   const option = document.createElement('option');
+                   option.value = name;
+                   option.textContent = name;
+
+                   return option;
+                })
+        )
+    })
 
     return (data, state, action) => {
-        // @todo: #4.2 — обработать очистку поля
+        if (action && action.name === "clear") {
+            const button = document.querySelector('button[name="clear"]');
+            if (button) {
+                const parent = button?.parentElement;
+                const input = parent?.querySelector('input');
 
-        // @todo: #4.5 — отфильтровать данные используя компаратор
-        return data;
+                if (input) {
+                    input.value = '';
+                }
+
+                const fieldName = button?.dataset.field;
+                if (fieldName) {
+                    state[fieldName] = '';
+                }
+            }
+        }
+
+        return data.filter(row => compare(row, state));
     }
 }
